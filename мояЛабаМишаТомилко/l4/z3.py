5bd734580e60bb1e32720f867c6b4f84e750785b
MihailTomilko <mishatomilko@yandex.ru>
2024-12-06 01:53:05 +0500

    теперь точно

--- мояЛабаМишаТомилко/l4/z3.py
+++ мояЛабаМишаТомилко/l4/z3.py
@@ -1,5 +1,8 @@
 from random import randint
-c = int(input("Введите длину списка: "))
-li = [randint(-20, 20) for i in range(c)]
-print(li)
-print(max(max(li[:int(len(li)/2)]), max(li[int(len(li)/2):])))
+try:
+    c = int(input("Введите длину списка: "))
+    li = [randint(-20, 20) for i in range(c)]
+    print(li)
+    print(max(max(li[:int(len(li)/2)]), max(li[int(len(li)/2):])))
+except:
+    print("Неверно введены значения")
