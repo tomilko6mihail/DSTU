9368c5af8f5d5859d43f8a47e745563f4310a1bd
MihailTomilko <mishatomilko@yandex.ru>
2025-02-14 11:42:39 +0300

    upd
    u[d

--- мояЛабаМишаТомилко/semak2/l1/labaDSTU/labaDSTU/main.cpp
+++ мояЛабаМишаТомилко/semak2/l1/labaDSTU/labaDSTU/main.cpp
@@ -18,6 +18,45 @@ float find_Y(float x){
     }
     return 0;
 }
+
+bool validation(const string str){
+    bool flagDot = false;
+    if (str.empty()){
+        cout << "Строка не может быть пустой" << endl;
+        return false;
+    }
+    int startIndex = str[0] == '-' ? 1 : 0;
+    
+    for (int i = startIndex; i < str.length(); i++ ){
+        if(str[0] == '.'){
+            cout << "Число не должно начинаться с точки" << endl;
+            return false;
+        }
+        if(isspace(str[i])){
+            cout << "Число не должно содержать пробелы" << endl;
+            return false;
+        }
+        if (str[i] == '.'){
+            if(flagDot){
+                cout << "В числе может быть лишь одна точка" << endl;
+                return false;
+            }
+            flagDot = true;
+        }
+        else if (!isdigit(str[i])){
+            cout << "В числе не могут быть буквы" << endl;
+            return false;
+        }
+    }
+    if(startIndex < str.length()){
+        return true;
+    }
+    else{
+        cout << "Минус это не число" << endl;
+        return false;
+    }
+}
+
 int main() {
     met1:
     float x, y;
@@ -25,25 +64,34 @@ int main() {
     cinx:
     cout << "Введите х: ";
     cin >> xStr;
-    try{
+    if(validation(xStr)){
         x = stof(xStr);
-    }
-    catch(invalid_argument){
-        cout << "Некорректный формат данных!" << endl;
-        goto cinx;
+    }else{
+        goto met1;
     }
     cout << x << endl;
     ciny:
     cout << "Введите у: ";
     cin >> yStr;
-    try{
+    if(validation(yStr)){
         y = stof(yStr);
-    }
-    catch(invalid_argument){
-        cout << "Некорректный формат данных!" << endl;
-        goto ciny;
+    }else{
+        goto met1;
     }
     cout << "Значение выражения для Z = " << find_Z(x, y) << endl;
     cout << "Значение выражения для Y = " << find_Y(x) << endl;
     goto met1;
 }
+///легендарное обращение пацам впр 11
+///
+  /// Мишка красавчик
+/// Янаков в подвале
+/// Ядерная установка Томилко-Янакова
+/// Томифулин
+/// Гарифуленко
+/// 9 общага горит
+/// С 23 февраля, любимые, подарка не будет, вас слишком много (в качестве подарка бомбим 9 общагу) (и 10 тоже)
+///
+///  но мы всё равно вас любим
+///  но это не точно
+///  например юру не любим
